5ef6973aedb0bf7becb83c04a121e57465a76a2b
mazen <mazenkhtab11@gmail.com>
2024-12-25 14:03:15 +0200

    update2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Home from "./pages/home";
+import HTML from "./pages/html";
+import Css from "./pages/css";
+import JS from "./pages/JS";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    errorElement: <h2 className="errorMessage">This Page Not Found!!!</h2>,
+  },
+
+  {
+    path: "/html",
+    element: <HTML></HTML>,
+  },
+
+  {
+    path: "/Css",
+    element: <Css></Css>,
+  },
+
+  {
+    path: "/javascript",
+    element: <JS></JS>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
